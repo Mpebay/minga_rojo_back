5ef00a2ac53164b8fd15a8be60eb71f77f905700
Benjamin Sepulveda <benjaminsepulveda.0420@gmail.com>
2023-09-07 19:36:21 -0300

    xd

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import userRouter from "./users.js"
+import mangasRouter from "./mangas.js"
+import categoriesRouter from "./categories.js"
+
 let router = express.Router();
 
 router.use("/", userRouter)
+router.use("/mangas", mangasRouter)
+router.use("/categories", categoriesRouter)
+
 
 export default router;
