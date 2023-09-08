c64648b32ec01fb1ba568324ecb438cff01eb3dd
juan pablo romero <jpr.1992@hotmail.com>
2023-09-08 17:52:51 -0300

    pechito

--- routes/chapters.js
+++ routes/chapters.js
@@ -1,10 +1,9 @@
-import express from 'express';
-import read from '../controllers/chapters/read.js';
-import get_one from '../controllers/chapters/get_one.js';
-import getChapters  from '../cotrollers/chapters/chapterControllers.js';
+import express from "express";
+import get_one from "../controllers/chapters/get_one.js";
+import getChapters from "../cotrollers/chapters/chapterControllers.js";
 const router = express.Router();
 
-router.get('/:id' , get_one );
-router.get('/', getChapters)
+router.get("/:id", get_one);
+router.get("/", getChapters);
 
 export default router;
