4e5afe87e8b6031cf48c62161c61de47bd5f008a
Santiago <santiagonlaghi@hotmail.com>
2023-09-16 15:28:03 -0300

    sprint 3 terminado

---
+++ middlewares/is_property_of.js
@@ -0,0 +1,17 @@
+import Manga from '../models/manga.js';
+
+const isPropertyOf = (req, res, next) => {
+    const mangaId = req.params.mangaId;
+    const manga = Manga.findById(mangaId);
+    if (manga.author_id === req.author._id) {
+        next();
+    } else {
+        res.status(401).json({
+            success: false,
+            response: null,
+            message: 'You are not authorized to perform this action'
+        })
+    }
+}
+
+export default isPropertyOf;
--- routes/chapters.js
+++ routes/chapters.js
@@ -1,9 +1,17 @@
 import express from "express";
 import get_one from "../controllers/chapters/get_one.js";
 import getChapters from "../controllers/chapters/chapterControllers.js";
+import validator from "../middlewares/validator.js";
+import chapterValidator from "../validators/chapterValidator.js";
+import addCoverPhoto from "../middlewares/add_cover_photo.js";
+import create from "../controllers/chapters/create.js";
+// import passport from "../middlewares/passport.js";
+// import isPropertyOf from "../middlewares/is_property_of.js";
+
 const router = express.Router();
 
 router.get("/:id", get_one);
 router.get("/", getChapters);
+router.post ("/", /*passport.authenticate("jwt", {session:false})*/ /*isPropertyOf*/ addCoverPhoto, validator(chapterValidator), create)
 
 export default router;
