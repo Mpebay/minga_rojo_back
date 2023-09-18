e6573bbcf1fea5ba1afed20b3adb3b686116d15f
Manuel Pebay <manupebay@hotmail.com>
2023-09-18 18:23:57 -0300

    A la chucha2

---
+++ controllers/auth/signin.js
@@ -0,0 +1,33 @@
+import User from "../../models/User.js";
+import Author from "../../models/Author.js"
+
+
+export default async (req, res, next) => {
+    try {
+       
+        let user = await User.findOneAndUpdate({ email: req.body.email }, { online: true }, { new: true });
+        let author = await Author.findOne({user_id: user._id})
+        if (!user) {
+          
+            return res.status(401).json({
+                message: 'Authentication failed',
+                success: false
+            });
+        }
+
+        const userData = {
+            email: user.email,
+            photo: user.photo,
+            role: user.role,
+            author: author._id
+        };
+
+        return res.status(200).json({
+            response: { token: req.token, user: userData },
+            message: 'User signed in with token',
+            success: true
+        });
+    } catch (error) {
+        next(error);
+    }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import read from "../controllers/auth/read.js";
+import signin from "../controllers/auth/signin.js";
+import accountNotExists from "../milddleware/accountNotExists.js";
+import passwordIsOk from "../milddleware/passwordIsOk.js";
+import generateToken from "../milddleware/generateToken.js";
+import userValidator from "../validators/userValidator.js";
+import validator from "../milddleware/validator.js";
+import passport from "../milddleware/passport.js";
+import signout from "../controllers/auth/signout.js";
+
 
 const router = Router();
 router.get("/", read);
+router.post("/signin",validator(userValidator), accountNotExists, passwordIsOk, generateToken, signin )
+router.post("/signout", passport.authenticate("jwt", {session:false}), signout)
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
